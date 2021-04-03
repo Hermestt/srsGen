c922b361aec024b01a70fb0fd89da11b4cb1225d
Pedro <trailblazering@protonmail.com>
2021-04-03 17:02:27 +0100

    fix of population on update

--- client/src/components/Document/DocumentUpdate.js
+++ client/src/components/Document/DocumentUpdate.js
@@ -18,42 +18,102 @@ import AuthService from "../../services/AuthService";
 import "./DocumentFormContainer.css";
 
 function DocumentUpdate() {
-  const { document, setDocument } = useContext(documentContext);
   const history = useHistory();
   let { id } = useParams();
 
-  //const { documentValue, setDocumentValue } = useContext(documentContext);
   const [documentData, setDocumentData] = useState(null);
 
-  /*
+  {
+    /* STATEEES */
+  }
+  const [documentInfo, setDocumentInfo] = useState({});
+  const [documentGPV, setDocumentGPV] = useState({});
+  const [documentStories, setDocumentStories] = useState([]);
+  const [documentPages, setDocumentPages] = useState([]);
+  const [documentBackend, setDocumentBackend] = useState([]);
+  const [documentFrontend, setDocumentFrontend] = useState([]);
+  const [documentSecurity, setDocumentSecurity] = useState([]);
+  const [documentLibraries, setDocumentLibrary] = useState([]);
+  const [documentTBR, setDocumentTBR] = useState([]);
+  const [documentFeatures, setDocumentFeatures] = useState([]);
+
   useEffect(() => {
     async function fetchDocument() {
       console.log("Document value in update is " + id);
-      const givenDocument = await DocumentService.getDocument(id);
-      setDocumentData(givenDocument);
+      const doc = await DocumentService.getDocument(id);
+      setDocumentData(doc);
     }
     fetchDocument();
   }, []);
-*/
-  const onInputChange = (childName, childValue) => {
-    setDocumentData({ ...document, [childName]: childValue });
+
+  const onInfoChange = (childName, childValue) => {
+    setDocumentInfo({ ...documentInfo, [childName]: childValue });
+  };
+  const onGPVChange = (childName, childValue) => {
+    setDocumentGPV({ ...documentGPV, [childName]: childValue });
+  };
+
+  const onStoryChange = (array) => {
+    setDocumentStories(array);
+  };
+  const onPageChange = (array) => {
+    setDocumentPages(array);
+  };
+  const onBackendChange = (array) => {
+    setDocumentBackend(array);
+  };
+  const onFrontendChange = (array) => {
+    setDocumentFrontend(array);
+  };
+  const onSecurityChange = (array) => {
+    setDocumentSecurity(array);
+  };
+  const onLibraryChange = (array) => {
+    setDocumentLibrary(array);
+  };
+  const onFeatureChange = (array) => {
+    setDocumentFeatures(array);
+  };
+
+  const onTBRChange = (childName, childValue) => {
+    setDocumentTBR({ ...documentTBR, [childName]: childValue });
   };
 
   const handleSubmit = async () => {
+    const doc = factory();
+    console.log("doc is ");
+    console.log(doc);
+    await DocumentService.updateDocument(doc);
+    history.push("/");
+  };
+
+  const factory = () => {
     let documentPack = {
       creator_id: AuthService.auth.user.id,
-      _id: id,
-      name: document.name,
-      description: document.description,
+      name: documentInfo.name,
+      description: documentInfo.description || "",
       goalsAndDescription: {
-        goals: document.goals,
-        problemsToSolve: document.problems,
-        vision: document.vision,
+        goals: documentGPV.goals || "",
+        problems: documentGPV.problems || "",
+        vision: documentGPV.vision || "",
       },
+      userStories: documentStories,
+      tech: {
+        backend: documentBackend,
+        frontend: documentFrontend,
+        security: documentSecurity,
+        libraries: documentLibraries,
+      },
+      pages: documentPages,
+      timeBudgetRisks: {
+        timeline: documentTBR.timeline,
+        budget: documentTBR.budget,
+        risks: documentTBR.risks,
+      },
+      features: documentFeatures,
     };
     console.log(documentPack);
-    await DocumentService.updateDocument(documentPack);
-    history.push("/");
+    return documentPack;
   };
 
   return (
@@ -62,7 +122,42 @@ function DocumentUpdate() {
       <div className="document-form_container">
         <Row className="justify-content-md-center">
           <Col sm={8}>
-            <DocumentForm onFormChange={onInputChange} document={document} />
+            <DocumentForm
+              onInfoChange={onInfoChange}
+              onGPVChange={onGPVChange}
+              onTBRChange={onTBRChange}
+              onStoryChange={onStoryChange}
+              onPageChange={onPageChange}
+              onBackendChange={onBackendChange}
+              onFrontendChange={onFrontendChange}
+              onSecurityChange={onSecurityChange}
+              onLibraryChange={onLibraryChange}
+              onFeatureChange={onFeatureChange}
+              document={
+                documentData || {
+                  ...documentInfo,
+                  goalsAndDescriptions: {
+                    goals: documentGPV.goals,
+                    problems: documentGPV.problems,
+                    vision: documentGPV.vision,
+                  },
+                  tech: {
+                    backend: documentBackend,
+                    frontend: documentFrontend,
+                    security: documentSecurity,
+                    libraries: documentLibraries,
+                  },
+                  userStories: documentStories,
+                  pages: documentPages,
+                  timeBudgetRisks: {
+                    timeline: documentTBR.timeline,
+                    budget: documentTBR.budget,
+                    risks: documentTBR.risks,
+                  },
+                  features: documentFeatures,
+                }
+              }
+            />
           </Col>
         </Row>
         <Row className="justify-content-md-center">
