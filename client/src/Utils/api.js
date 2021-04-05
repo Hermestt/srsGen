32b7e0d68ae250a4764fe2f32fcba08108d191c8
Pedro <trailblazering@protonmail.com>
2021-04-05 21:58:43 +0100

    fix env

--- client/src/Utils/api.js
+++ client/src/Utils/api.js
@@ -1,3 +1,8 @@
 export const getApiPath = (path) => {
-  return process.env.REACT_APP_API_URL + path;
+  let apiUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_PROD_API_URL
+      : process.env.REACT_APP_DEV_API_URL;
+  console.log("apiUrl: " + apiUrl);
+  return apiUrl + path;
 };
