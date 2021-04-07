db2b7e287644307e838c9510aac5586b47f603d4
Pedro <trailblazering@protonmail.com>
2021-04-07 19:19:47 +0100

    User and Auth api doc

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authRouter = express.Router();
 const hasBody = require("../middlewares/hasBody.js");
-const handleLogin = require("../controllers/authController");
+const handleLogin = require("../controllers/authController/authController");
 
 authRouter.post("/login", hasBody, handleLogin);
 
