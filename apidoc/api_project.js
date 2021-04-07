d0a83261d6bd05bd7ab40635674c9fa734a433c3
Pedro <trailblazering@protonmail.com>
2021-04-07 01:37:22 +0100

    firsrt steps on apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "example",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-07T00:24:36.471Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
--- server/controllers/documentController.js
+++ server/controllers/documentController.js
@@ -12,7 +12,19 @@ async function handleDocumentCreation(req, res) {
   });
 }
 
-// handleDocumentListing
+/**
+ * @api {get} /listing/:id Request User specific documents
+ * @apiName Get User Documents
+ * @apiGroup Documents
+ * @apiVersion 0.2.0
+ *
+ * @apiSuccess {Array} array The documents from the user with the :id.
+ *
+ * @apiSuccessExample Example data on success:
+ * list: [
+ *     { _id: "606a2959b9581a2960d1295f", title: "test", description: "" }
+ * ]
+ */
 async function handleDocumentListing(req, res) {
   const data = req.params.id;
   const list = await DocumentModel.find({ creator_id: data }).select(
@@ -31,7 +43,31 @@ async function handleDocumentDeletion(req, res) {
   });
 }
 
-// handleDocumentDelivery
+/**
+ * @api {get} /read/:id Request Document information
+ * @apiName GetDocument
+ * @apiGroup Documents
+ * @apiVersion 0.2.0
+ *
+ * @apiSuccess {Object} object The document object.
+ *
+ * @apiSuccessExample Example data on success:
+ * {
+ *  title: "New project",
+ *  description: "This project is meant for learning",
+ *  goals: "Learn about project development",
+ *  problems: "The editors don't have music integrated",
+ *  vision: "Make music out of code",
+ *  userStories: [
+ *    {id: "1", who: "Musician", wants: "code", objective: "make code music"}
+ *  ],
+ *  backend: [
+ *     {id: "1", description: "Node.js"}
+ *  ],
+ *  frontend: [
+ *     {id: "1", description: "React"}
+ *  ]
+ */
 async function handleDocumentDelivery(req, res) {
   const data = req.params.id;
 
