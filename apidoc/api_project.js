aec1a65632c2741858f67ab7e31659c56c157820
Pedro <trailblazering@protonmail.com>
2021-04-07 18:42:56 +0100

    document API

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,13 +1,13 @@
 define({
-  "name": "example",
+  "name": "SRSGen",
   "version": "0.1.0",
-  "description": "apiDoc basic example",
+  "description": "REST API docs of SRSGen",
   "apidoc": "0.3.0",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-04-07T00:24:36.471Z",
+    "time": "2021-04-07T17:40:22.016Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
