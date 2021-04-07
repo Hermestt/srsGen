88b7da18d5cb7d3581c15adb6a69dbd638150648
Pedro <trailblazering@protonmail.com>
2021-04-07 23:39:52 +0100

    fix link

--- client/src/Utils/api.js
+++ client/src/Utils/api.js
@@ -1,7 +1,4 @@
 export const getApiPath = (path) => {
-  let apiUrl =
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_PROD_API_URL
-      : process.env.REACT_APP_DEV_API_URL;
+  let apiUrl = "https://srsgen.herokuapp.com/" + path;
   return apiUrl + path;
 };
