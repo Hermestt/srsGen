2023f03195b70bb652ff8e6d4e628fd05104bd5b
Pedro <trailblazering@protonmail.com>
2021-04-09 14:02:42 +0100

    landingpage and graphics update

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -9,6 +9,7 @@ import "./Landing.css";
 import logo from "../../Graphics/logo_blue.svg";
 import logoGrey from "../../Graphics/logo_grey.svg";
 import laptop from "../../Graphics/laptop.jpg";
+import mobile from "../../Graphics/mobile.jpg";
 import linkedin from "../../Graphics/linkedin.svg";
 import github from "../../Graphics/github.svg";
 
@@ -18,6 +19,7 @@ function Landing() {
       <div className="landing-message text-center">
         Website in continuos development, visit everyday for a new update.
       </div>
+      {/* NAVBAR ############################################################################# */}
       <Navbar className="d-flex justify-content-between">
         <Navbar.Brand href="/">
           <img src={logo} />
@@ -27,20 +29,25 @@ function Landing() {
         </Button>
       </Navbar>
       <Container>
-        <Row>
-          <Col sm={7}>
-            <h3>
+        {/* HEADER ############################################################################# */}
+        <Row className="text-center header-container align-items-center">
+          <Col lg={7} id="header-col1" className="text-lg-start  flex">
+            <h2 className="header-text fw-bolder">
               Your 'Drive' for Software Requirements Specification documents
-            </h3>
+            </h2>
             <Button href="/register">Try now, it's free</Button>
           </Col>
-          <Col sm={5}>
-            <img src={laptop} className="header-img" />
+          <Col lg={5} id="header-col2">
+            <img src={laptop} className="header-img laptop-img" />
+            <img src={mobile} className="header-img mobile-img" />
           </Col>
         </Row>
-        <Row className="d-flex justify-content-center text-center">
-          <Col sm={4}>
-            <h4>Why and what is a Software Requirement Specification?</h4>
+        {/* EXPLANATION ############################################################################# */}
+        <Row className="d-flex justify-content-center text-center explanation-container">
+          <Col md={10} lg={6}>
+            <h4 className="fw-bold" style={{ marginBottom: 16 }}>
+              Why and what is a Software Requirement Specification?
+            </h4>
             <p>
               Before going into grand adventures or building a house, we usually
               develop a plan unless we want to be trully adventurous. Developing
@@ -51,43 +58,50 @@ function Landing() {
             </p>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <h3>CREATE</h3>
+        {/* FEATURES ############################################################################# */}
+        <Row className="text-center justify-content-center features-container">
+          <Col lg="4" className="feature">
+            <h3 className="fw-bolder">CREATE</h3>
             <p>
               Create a document for each of your new idea and it'll be saved so
-              you can have access acros multiple devices
+              you can have access across multiple devices.
             </p>
           </Col>
-          <Col>
-            <h3>CREATE</h3>
+          <Col lg="4" className="feature">
+            <h3 className="fw-bolder">UPDATE</h3>
             <p>
-              Create a document for each of your new idea and it'll be saved so
-              you can have access acros multiple devices
+              Don't have everything figured out yet? No problem, as long as
+              you've saved the unfinished document you can come back later to
+              complete it.
             </p>
           </Col>
-          <Col>
-            <h3>CREATE</h3>
+          <Col lg="4" className="feature">
+            <h3 className="fw-bolder">DOWNLOAD</h3>
             <p>
-              Create a document for each of your new idea and it'll be saved so
-              you can have access acros multiple devices
+              Everything's set in stone? Congratulations! I bet you want to
+              share it with your team, or simply have handy on your device. Feel
+              free to download the document as a PDF.
             </p>
           </Col>
         </Row>
       </Container>
+      {/* CTA ############################################################################# */}
       <div className="cta-container text-center">
-        <h2 style={{ marginBottom: 32 }}>Better seen than said right?</h2>
+        <h2 style={{ marginBottom: 48 }} className="fw-bolder">
+          Better seen than said right?
+        </h2>
         <Button href="/register" variant="light">
           TRY FOR FREE
         </Button>
       </div>
+      {/* FOOTER ############################################################################# */}
       <div id="footer" className="text-center">
         <div>
           <a href="/" style={{ marginBottom: 24, display: "block" }}>
             <img src={logoGrey} />
           </a>
           <p>
-            Project developer by{" "}
+            Project developed by{" "}
             <a
               href="https://www.linkedin.com/in/pedromnoliveira/"
               target="_blank"
